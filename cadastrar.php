61d31d2cd2cb314ee8e5a2211ba6f6a10a30d4e3
Rafael Geronimo <rafaelgeronimo@outlook.com>
2018-10-26 13:31:41 -0300

    Implementado sistema de quantidade de convites

--- cadastrar.php
+++ cadastrar.php
@@ -3,13 +3,20 @@
     require 'config.php';
 
     if(!empty($_GET['codigo'])) {
-        $codigo = addslashes($_GET['codigo']);
-        $sql = "SELECT * FROM usuarios WHERE codigo = '$codigo'";
+        $codigo_convite = addslashes($_GET['codigo']);
+        $sql = "SELECT * FROM usuarios WHERE codigo = '$codigo_convite'";
         $sql = $pdo->query($sql);
         
         if($sql->rowCount() == 0) {
             header("Location: login.php");
             exit;
+        }else {
+            $convites = '';
+            $sql = "SELECT convites FROM usuarios WHERE codigo = '$codigo_convite'";
+            $sql = $pdo->query($sql);
+            $info = $sql->fetch();
+            $convites = $info['convites'];
+            echo "Convites restantes: ".$convites;
         }
     } else {
         header("Location: login.php");
@@ -23,9 +30,9 @@
         $sql = "SELECT * FROM usuarios WHERE email = '$email'";
         $sql = $pdo->query($sql);
 
-        if($sql->rowCount() <= 0) {
+        if($sql->rowCount() <= 0 && $convites > 0) {
             $codigo = md5(rand(0,99999).rand(0,99999));
-            $sql = "INSERT INTO usuarios (email, senha, codigo) VALUES ('$email', '$senha', '$codigo')";
+            $sql = "INSERT INTO usuarios (email, senha, codigo, convites) VALUES ('$email', '$senha', '$codigo', 5); UPDATE usuarios SET convites = convites - 1 WHERE codigo = '$codigo_convite';";
             $sql = $pdo->query($sql);
 
             unset($_SESSION['logado']);
--- php_registroporconvite.sql
+++ php_registroporconvite.sql
@@ -22,16 +22,21 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `email` varchar(100) DEFAULT '0',
   `senha` varchar(32) DEFAULT '0',
   `codigo` varchar(32) DEFAULT '0',
+  `convites` int(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela php_registroporconvite.usuarios: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela php_registroporconvite.usuarios: ~4 rows (aproximadamente)
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` (`id`, `email`, `senha`, `codigo`) VALUES
-	(1, 'rafaelgeronimo@outlook.com', 'e10adc3949ba59abbe56e057f20f883e', '123456'),
-	(3, 'mussum@live.com', 'e10adc3949ba59abbe56e057f20f883e', '5211254'),
-	(4, 'zacarias@live.com', 'e10adc3949ba59abbe56e057f20f883e', 'a989a884f9d73d5936e5569dd2f68301'),
-	(5, 'michael@outlook.com', '202cb962ac59075b964b07152d234b70', '66d3af9425e54c6ed2c6e90f5250793f');
+INSERT INTO `usuarios` (`id`, `email`, `senha`, `codigo`, `convites`) VALUES
+	(1, 'rafaelgeronimo@outlook.com', 'e10adc3949ba59abbe56e057f20f883e', '123456', 2),
+	(3, 'mussum@live.com', 'e10adc3949ba59abbe56e057f20f883e', '5211254', 5),
+	(4, 'zacarias@live.com', 'e10adc3949ba59abbe56e057f20f883e', 'a989a884f9d73d5936e5569dd2f68301', 5),
+	(5, 'michael@outlook.com', '202cb962ac59075b964b07152d234b70', '66d3af9425e54c6ed2c6e90f5250793f', 5),
+	(7, 'roberto@update.com', 'caf1a3dfb505ffed0d024130f58c5cfa', '8e621e1c39f181e0a4484988d01f5c60', 5),
+	(8, 'astolfo@bistro.com', 'e10adc3949ba59abbe56e057f20f883e', 'a7915919d98f25498606783f5b6c32a7', 4),
+	(9, 'abc@abc.com', '202cb962ac59075b964b07152d234b70', '730c73180ba15524e60726e551847756', 4),
+	(10, 'fulano@hotmail.com', '4531e8924edde928f341f7df3ab36c70', '51dc23f1b91f4768fb18457ca7f9bd32', 5);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
